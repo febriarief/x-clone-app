76a98762068eabbd959db2637467845ebafaaf30
Febri Arief <febri@idevarts.com>
2025-10-05 03:48:30 +0700

    like and delete post functionality added

--- mobile/utils/api.ts
+++ mobile/utils/api.ts
@@ -23,6 +23,18 @@ export const useApiClient = (): AxiosInstance => {
 
 export const userApi = {
 	syncUser: (api: AxiosInstance) => api.post('/users/sync', {}),
-	getCurentUser: (api: AxiosInstance) => api.post('/users/me'),
+	getCurentUser: (api: AxiosInstance) => api.get('/users/me'),
 	updateProfile: (api: AxiosInstance, data: any) => api.put('/users/profile', data)
+};
+
+export const postApi = {
+	createPost: (api: AxiosInstance, data: {content: string; image?: string}) => api.post('/posts', data),
+	getPosts: (api: AxiosInstance) => api.get('/posts'),
+	getUserPosts: (api: AxiosInstance, username: string) => api.get(`/posts/user/${username}`),
+	likePost: (api: AxiosInstance, postId: string) => api.post(`/posts/${postId}/like`),
+	deletePost: (api: AxiosInstance, postId: string) => api.delete(`/posts/${postId}`),
+};
+
+export const commentApi = {
+	createComment: (api: AxiosInstance, postId: string, content: string) => api.post(`/comments/post/${postId}`, {content})
 };
