4274788826b184577c3d86e0cf3bc8d9d88b65ad
Febri Arief <febri@idevarts.com>
2025-10-04 14:40:36 +0700

    notifs and arcjet added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,31 @@
 import express from 'express';
 import cors from 'cors';
-import { clerkMiddleware } from '@clerk/express'; 
 
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
 
+import { clerkMiddleware } from '@clerk/express'; 
+import { arcjetMiddleware } from './middleware/arcjet.middleware.js';
+
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
+import notificationRoutes from './routes/notification.route.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
 
 app.get('/', (req, res) => res.send('Hello from server!'));
 
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentRoutes);
+app.use('/api/notifications', notificationRoutes);
 
 app.use((err, req, res, next) => {
     console.error('Unhandled error:', err);
