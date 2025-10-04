4c87769a7c40bc81f8efb02c4cb9fbea0985e3c6
Febri Arief <febri@idevarts.com>
2025-10-05 01:08:45 +0700

    user synced to db

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,14 +1,19 @@
 import { Stack } from 'expo-router';
 import { ClerkProvider } from '@clerk/clerk-expo';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '../global.css';
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <Stack screenOptions={{headerShown: false}}>
-        <Stack.Screen name="(auth)" options={{headerShown: false}} />
-      </Stack>
+      <QueryClientProvider client={queryClient}>
+        <Stack screenOptions={{headerShown: false}}>
+          <Stack.Screen name="(auth)" />
+        </Stack>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 }
